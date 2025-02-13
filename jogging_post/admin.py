2ca448496ff159045f7fa0dcca840bc2c128a835
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2025-02-13 15:44:55 +0000

    Adjusted Post and Comment Models to fix admin formatting

--- jogging_post/admin.py
+++ jogging_post/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.models import User
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = '__all__'
+    
+    def __str__(self):
+        return self.title
+    
+@admin.register(Comment)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+    
+    def __str__(self):
+        return self.title
