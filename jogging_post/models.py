5b671d5f3bdeea000c8f72a874dcebe0eb06e198
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2025-02-15 14:41:56 +0000

    Add Banner and Background Card Images

--- jogging_post/models.py
+++ jogging_post/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from cloudinary.models import CloudinaryField
 
-
 TYPE_CHOICES = [
         ('Urban', 'Urban'),
         ('Trail', 'Trail'),
@@ -67,21 +66,21 @@ class Post(models.Model):
         elif self.type == 'Coffee':
             self.icon = 'fa-solid fa-mug-hot'
 
-        # if self.type == 'Urban':
-        #     self.background_image = 'static/images/cards/digbethbw.jpg'
-        #     self.banner_image = 'static/images/banners/digbeth.webp'
-        # elif self.type == 'Trail':
-        #     self.background_image = 'static/images/cards/CannonHillBW.jpg'
-        #     self.banner_image = 'static/images/banners/cannon-hill-running.webp'
-        # elif self.type == 'Track':
-        #     self.background_image = 'static/images/cards/AlexanderStadiumBW.jpg'
-        #     self.banner_image = 'static/images/banners/Alexander-Stadium.webp'
-        # elif self.type == 'Canals':
-        #     self.background_image = 'static/images/cards/CanalsBW.jpg'
-        #     self.banner_image = 'static/images/banners/canals.webp'
-        # elif self.type == 'Coffee':
-        #     self.background_image = 'static/images/cards/McDRampBW.jpg'
-        #     self.banner_image = 'static/images/banners/McDRamp.webp'
+        if self.type == 'Urban':
+            self.background_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739548545/wx8v7eobfqnybfqv1qe6.jpg'
+            self.banner_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739554324/msatdulhap7usflyew1h.jpg'
+        elif self.type == 'Trail':
+            self.background_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739548560/wdet326lfb3kxhg7e4zi.jpg'
+            self.banner_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739554879/noxbbt4p8rcbeucaw4ll.webp'
+        elif self.type == 'Track':
+            self.background_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739548567/bfxyszqdwht1yzgs4zqd.jpg'
+            self.banner_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739554368/kp9wpvtezagqchlihbid.webp'
+        elif self.type == 'Canals':
+            self.background_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739548574/e3tjck3dxm5loyllyrqw.jpg'
+            self.banner_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739554376/xdw41j4m7a4cq6xrfsbk.jpg'
+        elif self.type == 'Coffee':
+            self.background_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739548580/eiw4yycwosrmizpvovgl.jpg'
+            self.banner_image = 'https://res.cloudinary.com/didwpm5uk/image/upload/v1739554383/utc1xbhssud4rr2vundk.webp'
 
         self.title = f"Jogging Event {self.type}"
         self.day_int = day_mapping.get(self.day, 0)  # Default to 0 if day is not found
