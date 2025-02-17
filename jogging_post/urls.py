416d2178b6a9fac62759969024e5ee60c921f69f
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2025-02-17 15:53:31 +0000

    Removed all old commented out code as well as deleted any py files not needed anymore or empty

--- jogging_post/urls.py
+++ jogging_post/urls.py
@@ -1,10 +1,6 @@
-from . import views
 from django.urls import path
 from .views import post_detail
-from profiles.views import ProfileUpdateView, ProfileDeleteView
 
 urlpatterns = [
     path('post/<slug:slug>/', post_detail, name='post_detail'),
-    # path('<slug:slug>/edit_comment/<int:comment_id>', views.ProfileUpdateView, name='comment_edit'),
-    # path('<slug:slug>/delete_comment/<int:comment_id>', views.ProfileDeleteView, name='comment_delete'),
 ]
